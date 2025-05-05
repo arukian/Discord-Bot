3f115c75e3ba61128dd5b0e8ef486618ae991ea8
arukian <35908690+arukian@users.noreply.github.com>
2025-05-04 21:32:39 -0600

    config.js

--- config.js
+++ config.js
@@ -19,8 +19,8 @@ module.exports = {
      {
       name: "GlaceYT",
       password: "glaceyt",
-      host: "193.226.78.187",
-      port:  9372,
+      host: "digi.pylex.xyz",
+      port:  11703,
       secure: false
     }
   ]
